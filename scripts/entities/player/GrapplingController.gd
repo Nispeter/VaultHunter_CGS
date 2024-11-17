2fd700b6e737e53bbf49a4de1f2d8c264f01e9df
Nispeter <Nispeter2@gmail.com>
2024-11-17 19:42:01 -0300

    fully working hook, missing horizontal momentum and swing

--- scripts/entities/player/GrapplingController.gd
+++ scripts/entities/player/GrapplingController.gd
@@ -5,34 +5,34 @@ extends Node2D
 
 var is_hooked: bool = false
 var grapple_speed: float = 500.0
-var grapple_max_distance: int = 500
-var grapple_min_distance: int = 10
+var swing_damping = 0.95
+var grapple_max_distance: int = 200
+var grapple_min_distance: int = 4
 var grapple_current_distance: int
 var hook_pos: Vector2 = Vector2.ZERO
 var motion: Vector2 = Vector2.ZERO
-
 func _ready() -> void:
 	grapple_current_distance = grapple_max_distance
 
 func _physics_process(delta: float) -> void:
 	hook()
-
+	queue_redraw()				#TODO: search a better method for drawing a hook, like texture tiling
 	if is_hooked:
 		swing(delta)
-		_draw()
-		motion *= 0.975  # Swing damping
+		motion *= swing_damping 
 		player_body.velocity = motion
 	else:
 		motion = player_body.velocity
 
 func _draw() -> void:
 	if is_hooked:
-		draw_line(Vector2(0, -16), to_local(hook_pos), Color.WHITE, 3, true)
+		print("drawing line")
+		draw_line(Vector2(4,0), to_local(hook_pos), Color.WHITE, 3, true)
 	else:
 		if raycast_angles is RayCast2D and raycast_angles.is_colliding():
 			var collision_point = raycast_angles.get_collision_point()
 			if global_position.distance_to(collision_point) < grapple_max_distance:
-				draw_line(Vector2(0, -16), to_local(collision_point), Color.BLACK, 0.5, true)
+				draw_line(Vector2(4,0), to_local(collision_point), Color.BLACK, 0.5, true)
 
 func hook() -> void:
 	raycast_angles.look_at(get_global_mouse_position())
@@ -50,19 +50,21 @@ func hook() -> void:
 func get_hook_pos() -> Vector2:
 	for raycast in raycast_angles.get_children():
 		if raycast is RayCast2D and raycast.is_colliding():
-			return raycast.get_collision_point()
+			var collider = raycast.get_collider()
+			if collider and not collider.is_in_group("Player"):
+				return raycast.get_collision_point()
 	return Vector2.ZERO
 
-func swing(delta: float) -> void:
+func swing(delta: float) -> void: 
 	var radius: Vector2 = global_position - hook_pos
 	if motion.length() < 0.01 or radius.length() < grapple_min_distance:
 		return
 
-	var angle: float = radius.angle_to(motion)
+	var angle: float = acos(radius.dot(motion))/(radius.length() * motion.length())
 	var radial_velocity: float = cos(angle) * motion.length()
-	motion = radius.normalized() * radial_velocity
+	motion = radius.normalized() * -radial_velocity
 
 	if global_position.distance_to(hook_pos) > grapple_current_distance:
-		global_position = hook_pos + radius.normalized() * grapple_current_distance
+		player_body.global_position = hook_pos + radius.normalized() * grapple_current_distance
 
 	motion += (hook_pos - global_position).normalized() * 15000.0 * delta
